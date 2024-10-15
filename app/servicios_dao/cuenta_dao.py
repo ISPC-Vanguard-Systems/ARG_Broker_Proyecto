e57c9c3c840fa644d8d7d02b114d7e05605348c4
AAMinoldo <minoldoaldo@gmial.com>
2024-10-15 12:02:23 -0300

    agrego clase cuenta, cuenta_dao

---
+++ app/servicios_dao/cuenta_dao.py
@@ -0,0 +1,36 @@
+from mysql.connector import Error
+from app.base_de_datos.conexion import Conexion
+from app.clases.cuenta import Cuenta
+
+
+def obtener_datos_cuenta(self, id_cuenta):
+    try:
+        conexion = Conexion()
+        cursor = conexion.cursor()
+
+        cursor.execute("SELECT numero_cuenta, saldo FROM cuentas WHERE id = %s",
+                       (id_cuenta))
+        datos = cursor.fetchone()
+
+        return datos
+    except Error as e:
+        print(f"Error al acceder a la base de datos: {e}")
+        return None
+    finally:
+        if cursor:
+            cursor.close()
+        if conexion:
+            conexion.close()
+
+
+def obtener_cuenta(self, id_cuenta):
+    datos = self.ob
+
+
+
+
+
+
+
+
+
