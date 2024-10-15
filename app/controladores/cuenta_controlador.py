0782a4d4b9709af84305b7c55ec16f5bebd0473c
AAMinoldo <minoldoaldo@gmial.com>
2024-10-15 15:00:13 -0300

    trabajo en controlador cuenta_controlador

--- app/controladores/cuenta_controlador.py
+++ app/controladores/cuenta_controlador.py
@@ -1,15 +1,21 @@
-from app.servicios_dao import cuenta_dao
+from app.servicios_dao.cuenta_dao import CuentaDao
+from app.clases.cuenta import Cuenta
 
-def mostrar_datos_cuenta(id_cuenta):
 
-    cuenta = cuenta_dao.obtener_datos_cuenta(id_cuenta)
+class CuentaControlador:
+    def __init__(self, acceso_db):
+        self.cuenta_dao  = CuentaDao(acceso_db)
+    def mostrar_datos_cuenta(self, id_cuenta):
 
-    if cuenta:
-        print(f"Numero de Cuenta: {cuenta_dao.Cuenta.get_numero_cuenta()}")
-        print(f"Saldo: {cuenta_dao.Cuenta.get_saldo()}")
+        datos = self.cuenta_dao.obtener_datos_cuenta(id_cuenta)
 
-    else:
-        print("Cuenta no econtrada")
+        if datos:
+            cuenta = Cuenta(id_cuenta, *datos)
+            print(f"Numero de Cuenta: {cuenta.get_numero_cuenta()}")
+            print(f"Saldo: {cuenta.get_saldo()}")
+
+        else:
+            print("Cuenta no econtrada")
 
 
 
--- app/servicios_dao/cuenta_dao.py
+++ app/servicios_dao/cuenta_dao.py
@@ -3,31 +3,34 @@ from app.base_de_datos.conexion import Conexion
 from app.clases.cuenta import Cuenta
 
 
-def obtener_datos_cuenta(id_cuenta):
+class CuentaDao:
 
-    conexion = Conexion()
-    cursor = conexion.obtener_cursor()
+    def obtener_datos_cuenta(self, id_cuenta):
 
-    if cursor is None:
-        return None #no se puede obtener cursos, termina aqui
+        conexion = Conexion()
+        cursor = conexion.obtener_cursor()
 
-    try:
-        query = "SELECT numero_cuenta, saldo FROM cuentas WHERE id_cuenta = %s"
-        cursor.execute(query, (id_cuenta,))#usar tupla con una coma
-        datos = cursor.fetchone()
-        return datos
-    except Error as e:
-        print(f"Error al acceder a la base de datos: {e}")
+        if cursor is None:
+            return None #no se puede obtener cursos, termina aqui
+
+        try:
+            query = "SELECT numero_cuenta, saldo FROM cuentas WHERE id_cuenta = %s"
+            cursor.execute(query, (id_cuenta,))#usar tupla con una coma
+            datos = cursor.fetchone()
+            return datos
+        except Error as e:
+            print(f"Error al acceder a la base de datos: {e}")
+            return None
+        finally:
+            cursor.close()
+            conexion.cerrar_conexion()
+
+
+    def obtener_cuenta(self, id_cuenta):
+        datos = self.obtener_datos_cuenta(id_cuenta)
+        if datos:
+            return Cuenta(id_cuenta, *datos)
         return None
-    finally:
-        cursor.close()
-        conexion.cerrar_conexion()
-
-def obtener_cuenta(id_cuenta):
-    datos = obtener_datos_cuenta(id_cuenta)
-    if datos:
-        return Cuenta(id_cuenta, *datos)
-    return None
 
 
 
