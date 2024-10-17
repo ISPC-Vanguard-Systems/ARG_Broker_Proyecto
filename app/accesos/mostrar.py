47a193cb2a100cd91509f4c6dee489480f1aca5b
AAMinoldo <minoldoaldo@gmial.com>
2024-10-17 00:07:07 -0300

    actaulizo

--- app/accesos/mostrar.py
+++ app/accesos/mostrar.py
@@ -1,6 +1,6 @@
 from app.base_de_datos.conexion import Conexion
 from app.controladores.cuenta_controlador import CuentaControlador
-from app.controladores.inversor_controller import iniciar_sesion, registrar_nuevo_inversor
+
 
 def ejecutar():
     acceso_bd = Conexion()
@@ -24,45 +24,4 @@ def ejecutar():
             print("Opcion no valida")
 
 
-# def ejecutar_menu():
-#     from app.accesos.mostrar import iniciar_sesion, registrar_nuevo_inversor  # Importación diferida
-#
-#     while True:
-#         print("\n--- MENÚ ---")
-#         print("1. Registrar inversor")
-#         print("2. Iniciar sesión")
-#         print("3. Salir")
-#
-#         opcion = input("Seleccione una opción: ")
-#
-#         if opcion == '1':
-#             registrar_nuevo_inversor()
-#         elif opcion == '2':
-#             if iniciar_sesion():  # Verifica el resultado de iniciar_sesion()
-#                 ejecutar()  # Llama a la función ejecutar() solo si fue exitoso
-#         elif opcion == '3':
-#             print("Saliendo...")
-#             break
-#         else:
-#             print("Opción no válida. Intente de nuevo.")
 
-# def ejecutar_menu():
-#     while True:
-#         print("\n--- MENÚ ---")
-#         print("1. Registrar inversor")
-#         print("2. Iniciar sesion")
-#         print("3. Salir")
-#         opcion = input("Seleccione una opción: ")
-#
-#         if opcion == '1':
-#             registrar_nuevo_inversor()
-#         elif opcion == '2':
-#             iniciar_sesion()
-#             bandera = 1
-#             if bandera == 1:
-#                 ejecutar()
-#         elif opcion == '3':
-#             print("Saliendo...")
-#             break
-#         else:
-#             print("Opción no válida. Intente de nuevo.")
