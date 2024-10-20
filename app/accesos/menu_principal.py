7c321bd7c7c1971d1660adc3afe1533684927bbf
AAMinoldo <minoldoaldo@gmial.com>
2024-10-19 21:00:58 -0300

    refactorizo el menu_principal

--- app/accesos/menu_principal.py
+++ app/accesos/menu_principal.py
@@ -1,4 +1,5 @@
 from app.controladores.inversor_controller import Inversor_Controller
+from app.accesos.mostrar import ejecutar
 
 def ejecutar_menu():
     inversor_ctrl = Inversor_Controller()
@@ -13,7 +14,8 @@ def ejecutar_menu():
         if opcion == '1':
             inversor_ctrl.registrar_nuevo_inversor()
         elif opcion == '2':
-            inversor_ctrl.iniciar_sesion()
+            if inversor_ctrl.iniciar_sesion():
+                ejecutar()
         elif opcion == '3':
             print("Saliendo...")
             break
