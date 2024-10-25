6cf0d800e5657f6ea282ce1bdb3b3aebd75703e5
AAMinoldo <minoldoaldo@gmial.com>
2024-10-25 18:58:45 -0300

    refactorizacion y mejora metodo cuenta

--- app/servicios_dao/cuenta_dao.py
+++ app/servicios_dao/cuenta_dao.py
@@ -35,11 +35,10 @@ class CuentaDao(InterfaceDAO):
         with Conexion() as conexion:
             query = """
                 SELECT a.simbolo,
-                       SUM(t.cantidad_acciones) AS total_acciones,
                        SUM(t.monto_total) AS monto_total,
                        SUM(t.comision) AS total_comision,
                        1000000 AS valor_inicial,
-                       (SUM(t.monto_total) - SUM(t.comision)) - 1000000 AS rendimiento
+                       (1000000 - SUM(t.monto_total) - SUM(t.comision)) AS rendimiento
                 FROM transacciones t 
                 INNER JOIN cuentas c ON t.numero_cuenta = c.numero_cuenta 
                 INNER JOIN acciones a ON t.id_accion = a.id_accion
@@ -47,8 +46,8 @@ class CuentaDao(InterfaceDAO):
                 GROUP BY a.simbolo
             """
             resultado = conexion.ejecutar_query(query, (id_cuenta,))
-            return [Transaccion(simbolo, total_acciones, monto_total, total_comision, valor_inicial, rendimiento) for
-                simbolo, total_acciones, monto_total, total_comision, valor_inicial, rendimiento in resultado] if resultado else []
+            return [Transaccion(simbolo, monto_total, total_comision, valor_inicial, rendimiento) for
+                simbolo, monto_total, total_comision, valor_inicial, rendimiento in resultado] if resultado else []
 
 
 
